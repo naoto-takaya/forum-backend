879c79af36b7ecaf8c2cc79442c2a6144b633ae4
Naoto Takaya <takayanaoto@takayanaotonoMacBook.local>
2019-12-02 23:32:26 +0900

    :+1: WebAPI用にファイル設定、circleciのデプロイ設定

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -37,24 +37,10 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->mapApiRoutes();
 
-        $this->mapWebRoutes();
 
         //
     }
 
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
-    }
 
     /**
      * Define the "api" routes for the application.
@@ -65,9 +51,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+        Route::middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
     }
 }

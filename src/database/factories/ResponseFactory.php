731add6481873881390e37af3277eeac0236bfa9
Naoto Takaya <bsb12solid@gmail.com>
2020-02-22 00:24:24 +0900

    :+1: レスポンス一覧にユーザー情報、レスポンスカウントを追加 (#45)

--- src/database/factories/ResponseFactory.php
+++ src/database/factories/ResponseFactory.php
@@ -10,12 +10,22 @@ use Faker\Generator as Faker;
 
 $factory->define(Response::class, function (Faker $faker) {
     return [
-        'forum_id' => factory(Forum::class)->create()->id,
+        'forum_id' => function () {
+            return factory(Forum::class)->create()->id;
+        },
         'content' => $faker->sentence(),
-        'user_id' => factory(User::class)->create()->id,
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
         'response_id' => null,
         'sentiment' => rand(1, 4),
         'is_deleted' => 0,
+    ];
+});
+
+$factory->state(Response::class, 'forum_chooseable', function (Faker $faker) {
+    return [
+        'forum_id' => null,
     ];
 });
 
